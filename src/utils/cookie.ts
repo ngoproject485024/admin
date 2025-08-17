894d5639d3deac2b317e82a55ac51e778a0bbc26
hossein khodakhah <hosseinkhodakhah6@gmail.com>
2025-08-17 23:50:35 +0330

    fix the comflicts

--- src/utils/cookie.ts
+++ src/utils/cookie.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Cookies from "universal-cookie";
 
 const cookies = new Cookies(null, { path: "/" });
@@ -16,22 +15,3 @@ export const getCookie = (name: string) => {
 export const removeCookie = (name: string) => {
   cookies.remove(name);
 };
-=======
-import Cookies from "universal-cookie";
-
-const cookies = new Cookies(null, { path: "/" });
-
-export const setCookie = (name: string, value: object, days: number) => {
-  const expirationDate = new Date();
-  expirationDate.setDate(expirationDate.getDate() + days);
-  cookies.set(name, value, { expires: expirationDate, secure: true });
-};
-
-export const getCookie = (name: string) => {
-  return cookies.get(name);
-};
-
-export const removeCookie = (name: string) => {
-  cookies.remove(name);
-};
->>>>>>> 3c0518d5f5fb1b6eba28e07cb359177f8d4b8ffc
