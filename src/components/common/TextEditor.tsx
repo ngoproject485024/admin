dc08e97ffa9b7e4be9ecbaa1292f2caa08883a65
hossein khodakhah <hosseinkhodakhah6@gmail.com>
2025-09-01 22:10:45 +0330

    change tiny mce

--- src/components/common/TextEditor.tsx
+++ src/components/common/TextEditor.tsx
@@ -53,8 +53,8 @@ function TextEditor({ title, formik, name, lang, onChange }: Props) {
   return (
     <ComponentCard title={title ? title : ""}>
       <Editor
-        apiKey="b1l0dr2vsao1zfzn9a90sj6nojthzf6zs5hfyo0rnnt2dqx4"
-        licenseKey="b1l0dr2vsao1zfzn9a90sj6nojthzf6zs5hfyo0rnnt2dqx4"
+        apiKey="c8j4rb46knr8u4c4bmjyp0ng723d9kdytesy4dyr3u4wf4oa"
+        licenseKey="c8j4rb46knr8u4c4bmjyp0ng723d9kdytesy4dyr3u4wf4oa"
         init={init}
         value={formik?.values?.[name] || formik?.values?.[name]}
         onInit={(evt, editor) => (editorRef.current = editor)}
